451253b361f71d9189c774a07fc493b846bdb435
PX201 <lahmamsiaiman99@gmail.com>
2026-02-24 22:28:34 -0800

    fix: fix Prisma import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "../app/generated/prisma/index.js";
+import { PrismaClient, Prisma } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 
