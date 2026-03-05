c37b10b202238bd07c4c9c4d4c7b1a2ef0879ae8
Al Cerdan Lico <alcerdan@yahoo.com>
2026-03-05 13:40:40 -0800

    fix api/projects to remove try catch

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -3,15 +3,8 @@ import { getProjects } from "@/src/features/projects/getProjects";
 import { createProject } from "@/src/features/projects/createProject";
 
 export const GET = route(async () => {
-
     const projects = await getProjects();
     return new Response(JSON.stringify(projects), { status: 200 });
-})
-
-export const POST = route(async (req: Request) => {
-    const body = await req.json()
-    const project = await createProject(body);
-    return new Response(JSON.stringify({ project }), { status: 201 })
 })
 
 export const POST = route(async (req: Request) => {
